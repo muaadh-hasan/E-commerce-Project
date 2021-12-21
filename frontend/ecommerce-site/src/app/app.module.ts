9382df643e4c6576f66f50aad4ffd3c2f6d4e98e
Muaadh Hasan <36192122+muaadh-hasan@users.noreply.github.com>
2021-12-21 21:16:34 +0200

    add logic for checkout forms and validations.

--- frontend/ecommerce-site/src/app/app.module.ts
+++ frontend/ecommerce-site/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +11,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
+import { ReactiveFormsModule } from '@angular/forms';
 // import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
@@ -18,12 +21,15 @@ import { SearchComponent } from './components/search/search.component';
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule
     // NgbModal
   ],
   providers: [],
--- frontend/ecommerce-site/src/app/services/cart.service.ts
+++ frontend/ecommerce-site/src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +9,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
 
    alreadyExistsInCart: boolean = false;
@@ -74,4 +74,30 @@ export class CartService {
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
     console.log('----');
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
+
 }
